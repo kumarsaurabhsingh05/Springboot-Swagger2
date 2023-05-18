d40b00fbade1dde6e089979e593a03bed9899a4a
kumarsaurabhsingh05 <108813273+kumarsaurabhsingh05@users.noreply.github.com>
2023-05-19 02:10:57 +0530

    Added Docket bean and basic annotation of Swagger

--- src/main/java/springboot/swagger2/SpringbootSwagger2Application.java
+++ src/main/java/springboot/swagger2/SpringbootSwagger2Application.java
@@ -2,12 +2,45 @@ package springboot.swagger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringbootSwagger2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSwagger2Application.class, args);
+	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("springboot.swagger2"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Address Book API",
+				"Sample API for Swagger tutorial",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("Saurabh Kumar", "https://github.com/kumarsaurabhsingh05", "kumarsaurabhsingh05@gmail.com"),
+				"API License",
+				"https://github.com/kumarsaurabhsingh05",
+				Collections.emptyList()
+		);
 	}
 
 }
